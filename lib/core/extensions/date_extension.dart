9f7aa808fa30febc5cd2461062ab6439bedd5952
mwlt68 <mwltgr@gmail.com>
2023-08-14 00:14:43 +0300

    Extensions add

---
+++ lib/core/extensions/date_extension.dart
@@ -0,0 +1,23 @@
+import 'package:intl/intl.dart';
+
+extension DateHelpers on DateTime {
+  bool isToday() {
+    final now = DateTime.now();
+    return now.day == this.day &&
+        now.month == this.month &&
+        now.year == this.year;
+  }
+
+  bool isYesterday() {
+    final yesterday = DateTime.now().subtract(Duration(days: 1));
+    return yesterday.day == this.day &&
+        yesterday.month == this.month &&
+        yesterday.year == this.year;
+  }
+
+  String getFormatted() {
+    return isToday()
+        ? DateFormat.yMd().format(this)
+        : DateFormat.Hm().format(this);
+  }
+}
