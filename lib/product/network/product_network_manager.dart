6f07ee93dda564f2aae730f44e6c62b1c54d077b
mwlt68 <mwltgr@gmail.com>
2023-08-27 23:47:40 +0300

    NetworkHelper add

--- lib/product/network/product_network_manager.dart
+++ lib/product/network/product_network_manager.dart
@@ -1,18 +1,11 @@
-import 'package:dio/dio.dart';
-import 'package:flutter/foundation.dart';
 import 'package:scandium/core/init/network/network_manager.dart';
 import 'package:scandium/core/init/storage/storage_manager.dart';
-import 'package:scandium/product/constants/application_constants.dart';
 import 'package:scandium/product/constants/storage_constants.dart';
+import 'package:scandium/product/helpers/network_helper.dart';
 import 'package:scandium/product/models/base/user.dart';
 
 class ProductNetworkManager extends NetworkManager {
-  ProductNetworkManager()
-      : super(
-            baseOptions: BaseOptions(
-                baseUrl: kIsWeb
-                    ? ApplicationConstants.instance.baseUrl
-                    : ApplicationConstants.instance.mobileBaseUrl));
+  ProductNetworkManager() : super(baseUrl: NetworkHelper.getBaseUrl);
 
   @override
   Future<String> getToken() async {
