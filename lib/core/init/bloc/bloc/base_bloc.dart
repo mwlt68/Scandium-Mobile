52f47f3138244c21800f7b2242aae1fd0b028951
mwlt68 <mwltgr@gmail.com>
2023-10-15 18:45:00 +0300

    EmitterHelper add

--- lib/core/init/bloc/bloc/base_bloc.dart
+++ lib/core/init/bloc/bloc/base_bloc.dart
@@ -1,9 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:scandium/core/base/models/base_response_model.dart';
-import 'package:scandium/core/base/models/mappable.dart';
 import 'package:scandium/core/init/bloc/model/base_bloc_dialog_model.dart';
-import 'package:scandium/product/constants/application_constants.dart';
 
 part 'base_event.dart';
 part 'base_state.dart';
@@ -11,39 +8,4 @@ part 'base_state.dart';
 class BaseBloc<TEvent extends BaseEvent, TState extends BaseState<TState>>
     extends Bloc<TEvent, TState> {
   BaseBloc(super.initialState);
-
-  bool emitBaseState<T extends IFromMappable, R extends BaseResponseModel<T>>(
-      Emitter<TState> emit, R? response,
-      {void Function()? whenSuccess}) {
-    if (response == null) {
-      emit(state.copyWithBase(
-          status: BaseStateStatus.success,
-          errorKeys: const ["ConnectionError"]));
-    } else if (!response.isValueNull() && response.hasNotError) {
-      emit(state
-          .copyWithBase(status: BaseStateStatus.success, errorKeys: const []));
-      if (whenSuccess != null) whenSuccess();
-      return true;
-    } else {
-      emit(state.copyWithBase(
-          status: BaseStateStatus.success,
-          errorKeys: response.errorContents?.map((e) => e.title!).toList() ??
-              [ApplicationConstants.instance.unexpectedErrorDefaultMessage]));
-    }
-    return false;
-  }
-
-  void emitErrorKeys(
-      Emitter<TState> emit, List<ErrorResponseContent>? errorContents) {
-    emit(state.copyWithBase(
-        status: BaseStateStatus.success,
-        errorKeys: errorContents?.map((e) => e.title!).toList() ??
-            [ApplicationConstants.instance.unexpectedErrorDefaultMessage]));
-  }
-
-  void emitSetLoading(Emitter<TState> emit, bool isLoading) {
-    emit(state.copyWithBase(
-      status: isLoading ? BaseStateStatus.loading : BaseStateStatus.success,
-    ));
-  }
 }
---
+++ lib/core/init/bloc/extension/emitter_extension.dart
@@ -0,0 +1,49 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:scandium/core/base/models/base_response_model.dart';
+import 'package:scandium/core/base/models/mappable.dart';
+import 'package:scandium/core/init/bloc/bloc/base_bloc.dart';
+import 'package:scandium/product/constants/application_constants.dart';
+
+extension EmitterHelper<TState extends BaseState<TState>> on Emitter<TState> {
+  Future<R?> emit<T extends IFromMappable, R extends BaseResponseModel<T>>(
+      {required TState state,
+      required Future<R?> requestOperation,
+      TState? Function(R)? getSuccessfulState,
+      bool isLoadingActive = true}) async {
+    if (isLoadingActive) {
+      this.emitSetLoading(state, true);
+    }
+    var response = await requestOperation;
+    if (response == null) {
+      this(state.copyWithBase(
+          status: BaseStateStatus.success,
+          errorKeys: const ["ConnectionError"]));
+    } else if (!response.isValueNull() && response.hasNotError) {
+      if (getSuccessfulState != null) {
+        var successState = getSuccessfulState(response);
+        successState ??= state;
+        this(successState.copyWithBase(
+            status: BaseStateStatus.success, errorKeys: const []));
+      }
+    } else {
+      this(state.copyWithBase(
+          status: BaseStateStatus.success,
+          errorKeys: response.errorContents?.map((e) => e.title!).toList() ??
+              [ApplicationConstants.instance.unexpectedErrorDefaultMessage]));
+    }
+    return response;
+  }
+
+  void emitErrorKeys(TState state, List<ErrorResponseContent>? errorContents) {
+    this(state.copyWithBase(
+        status: BaseStateStatus.success,
+        errorKeys: errorContents?.map((e) => e.title!).toList() ??
+            [ApplicationConstants.instance.unexpectedErrorDefaultMessage]));
+  }
+
+  void emitSetLoading(TState state, bool isLoading) {
+    this(state.copyWithBase(
+      status: isLoading ? BaseStateStatus.loading : BaseStateStatus.success,
+    ));
+  }
+}
